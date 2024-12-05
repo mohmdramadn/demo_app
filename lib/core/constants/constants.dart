766cb1055ce489ccabfdfe4ad4a5f0099b22790f
Mohamed Ramadan <o-muhammed.ramadan@eand.com.eg>
2024-12-05 03:15:22 +0200

    Added constant strings file

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  const Constants._();
+
+  static String get totalOrders => 'Total Orders';
+
+  static String get averagePrice => 'Average Price';
+
+  static String get numberOfReturns => 'Number Of Returns';
+
+  static String get showDiagram => 'Show Diagrams';
+}
